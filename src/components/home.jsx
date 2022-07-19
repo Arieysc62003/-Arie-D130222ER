00f730af98420e50202aca80ca4b4e4bc4821413
Arieysc62003 <arieysc62003@gmail.com>
2022-07-19 23:36:37 +0300

    victor change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,13 +4,14 @@ import PageHeader from "./pageHeader";
 class Home extends Component {
   state = {
     image: {
-      url: "https://cdn.babamail.co.il/Images/2020/4/14/29957a68-b8e3-4b66-8f72-968f01b28344.jpg",
-      alt: "blake horse",
+      url: "https://picsum.photos/1200/700",
+      alt: "Random image",
     },
   };
+
   render() {
     const { url, alt } = this.state.image;
-    return <img className="card-img-top" src={url} alt={alt} />;
+    return <img className="card-img-top" src={url} alt={alt} />
   }
 }
 
